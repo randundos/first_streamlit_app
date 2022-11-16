2e2a16633b5c3dcec978b5b04b49f1d6546344c4
randundos <38527626+randundos@users.noreply.github.com>
2022-11-16 16:42:27 +0100

    Update streamlit_app.py
    adding new things

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title('What a test')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 🥑Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
